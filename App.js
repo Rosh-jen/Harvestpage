8ac6bb37526a3fcedab6a72a6592e4d0e8ae0a5e
Rosh-jen <105044565+Rosh-jen@users.noreply.github.com>
2022-05-09 19:18:38 +1000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Harvest Page
+          
+      
+        </p>
+<b>What is the process of Harvesting?</b>
+Barely
+<c> Large crops of barely are usually harvested with large machinery such as 
+  tractors, but for small amounts of barley manual labour is best.
+  Tractor-driven combines used a shaker for grain separation, and a straw-walker would remove the straw. 
+  They were developed to have independent engines to power the grain separation
+   parts and gradually evolved into the combines we see today.</c>
+<d>Wheat</d>
+<e>
+When the wheat is ready, dry and fully ripened, the farmer uses a combine harvesting machine to collect the ripe grain.
+The harvester pours the grain into trucks.
+The combine harvester is driven through the paddocks where it cuts the wheat, 
+cleans and separates the seeds or kernels from the rest of the plant.
+</e>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
